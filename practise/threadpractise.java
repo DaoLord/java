7516cdb98d67b01bb69d826e5392e085e27f5baa
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-07-25 09:24:34 +0545

    Thread examples and practise

---
+++ practise/threadpractise.java
@@ -0,0 +1,16 @@
+class AA extends Thread{
+    public void run(){
+        System.out.println("hwllow world");
+    }
+}
+
+
+
+
+public class threadpractise {
+    public static void main(String[] args) {
+        AA a1= new AA(obj)
+        .start();
+    }
+    
+}
