9cccd4671ee52591d3b2fa4a3d0729a9c0ee864b
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-07-26 20:39:33 +0545

    String method

---
+++ practise/Third.java
@@ -0,0 +1,10 @@
+public class Third {
+  static void Concat( String name){
+    System.out.println(name +"Limbu");
+  }
+  public static void main(String[] args) {
+    Concat("Bishnu ");
+    Concat("Roshan ");
+    Concat("Subash ");
+  }
+}
