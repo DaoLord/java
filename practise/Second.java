83664f08eef83b1041e703a6a76e5a033f628152
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-07-26 20:29:52 +0545

    string array in java

---
+++ practise/Second.java
@@ -0,0 +1,8 @@
+public class Second {
+  public static void main(String[] args) {
+      String[] names={"Bishnu", "Roshan", "Hari"};
+      for(int i=0;i<3;i++){
+      System.out.println(names[i]);
+      }
+  }
+}
