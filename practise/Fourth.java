78a10748c88d923dc36f831d3f751b84ec1f3524
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-02 18:38:36 +0545

    just practis

---
+++ practise/Fourth.java
@@ -0,0 +1,16 @@
+public class A{
+ int a=10;
+}
+public class B extends A{
+  static void A(){
+  System.out.println(+a);
+  }
+}
+public class Fourth {
+  public static void main(String[] args) {
+    B obj= new B();
+    B();
+  }
+
+  
+}
