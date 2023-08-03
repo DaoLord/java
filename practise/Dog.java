aa549ccabadaf43f57ca9daca3cb01dfc09f56bf
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-03 23:42:29 +0545

    nono

--- practise/Dog.java
+++ practise/Dog.java
@@ -3,9 +3,4 @@ public class Dog {
   Scanner sac= new Scanner(System.in);
       String a= sac.nextLine();
   
-    public static void main(String[] args) {
-      System.out.println("Enter a string to display");
-      Dog obj1= new Dog();
-      System.out.println(obj1.a);
-    }
-}
+
