c6cb0d9cf4918e2520385a82312616ac9a12bdaa
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-08 21:19:03 +0545

    scanner practise

---
+++ practise/Try4.java
@@ -0,0 +1,16 @@
+/**
+ * Try4
+ */
+import java.util.Scanner;
+public class Try4 {
+  public static void main(String[] args) {
+    Scanner a = new Scanner(System.in);
+    System.out.println("Enter your first name:");
+    String fname = a.nextLine();
+    System.out.println("Enter your last name:");
+    String lname = a.nextLine();
+    System.out.print(fname +" "+lname);
+    
+  }
+
+}
