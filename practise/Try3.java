f02b949315446e203442cdbe71ce45cc56416943
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-08 20:45:14 +0545

    star pattern

---
+++ practise/Try3.java
@@ -0,0 +1,17 @@
+public class Try3 {
+  public static void main(String[] args) {
+    //     for(int i=1;i<=4;i++){
+    //       for(int j=1;j<=i;j++){
+    //         System.out.print("* ");
+    //       }
+    //       System.out.println();
+    //     }
+    //   }
+    // }
+    //
+    // java star pattern can also be done in this way.
+  System.out.println("*");
+  System.out.println("* *");
+  System.out.println("* * *");
+  System.out.println("* * * *");
+  }}
