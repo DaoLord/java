852e6b680ae568c08a88a279c423cc8f5d1831c2
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-16 19:10:56 +0545

    return type in method

---
+++ practise/try/newFolder/Try6.java
@@ -0,0 +1,12 @@
+/**
+ * Try6
+ */
+public class Try6 {
+    static int addNumber(int a){
+      return 100+a;  
+    }
+
+  public static void main(String[] args) {
+    System.out.println(addNumber(9));
+  }
+}
