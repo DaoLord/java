b55ce180ea6ac5fd74da98a87d6d9b800c44bf8a
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-16 18:59:06 +0545

    paramater passing in method(String)

---
+++ practise/try/newFolder/Try5.java
@@ -0,0 +1,14 @@
+/**
+ * Try5
+ */
+public class Try5 {
+  static void myName(String name){
+    System.out.println(name +" limbu");
+  }
+
+  public static void main(String[] args) {
+    myName("Subash"); 
+    myName("Roshan"); 
+    
+  }
+}
