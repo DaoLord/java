cdc273f2ce8ef0225a73f4401916e2600467eaf8
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-16 20:01:29 +0545

    start of iostream work

---
+++ IOStream java/first.java
@@ -0,0 +1,12 @@
+/**
+ * first
+ */
+import java.io.InputStream;
+public class first {
+
+  public static void main(String[] args) {
+    int i=System.in.read();
+    System.out.println((char)i);
+    
+  }
+}
