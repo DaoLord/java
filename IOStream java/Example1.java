991a71226025fd6e6b6f394a2f2f583ad3341c7c
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-19 19:03:25 +0545

    creates text.txt file and adds letters ABCDE

---
+++ IOStream java/Example1.java
@@ -0,0 +1,20 @@
+import java.io.FileOutputStream;
+public class Example1 {
+  public static void main(String[] args) {
+        
+    try{
+        FileOutputStream fout = new FileOutputStream ("text.txt");
+        fout.write(65);
+        fout.write(66);
+        fout.write(67);
+        fout.write(68);
+        fout.write(69);
+        fout.close();
+        System.out.println("successfull...");
+
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
+
