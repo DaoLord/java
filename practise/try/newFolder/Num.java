d289040009d6e941032093e8a01c70c60b9f6d9b
bishnu-limbu007 <subashlimbu987654321@gmail.com>
2023-08-22 21:56:02 +0545

    done

---
+++ practise/try/newFolder/Num.java
@@ -0,0 +1,28 @@
+public class Num {
+  public static void main(String[] args) {
+
+    // boolean a=true;
+    // System.out.println(a);
+    // System.out.println(~10);
+    // System.out.println(!a);
+    //
+    // short a=10;
+    // short b=10;
+    // a = a+b;
+    // System.out.println(a);
+// short a=10;  
+// short b=10;  
+// //a+=b;//a=a+b internally so fine  
+// a=a+b;//Compile time error because 10+10=20 now int  
+// System.out.println(a);  
+    String s = "roshan chor";
+    if(s.endsWith("limbu")){
+      System.out.println("yo ta roshan ho.");
+    }else if(s.endsWith("chor")){
+      System.out.println("yo ta roshan chor ho.");
+    }
+
+
+
+  }
+}
