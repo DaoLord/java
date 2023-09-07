f32ed1757cb8c31fc2451b30031b9414def0ef70
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-07 10:49:23 +0545

    adsf

--- practise/try/Trying.java
+++ practise/try/Trying.java
@@ -1,23 +1,24 @@
-public class trying {
-    // method to add two integers
+
+public class Trying {
+    // Method to add two integers
     public int add(int a, int b) {
         return a + b;
     }
 
-    // method to add two doubles
+    // Method to add two doubles
     public double add(double a, double b) {
         return a + b;
     }
 
-    public static void main(string[] args) {
-        calculator calculator = new calculator();
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
-        // using the add method with integers
-        int sum1 = calculator.add(5, 3); // calls the first add method (int version)
-        system.out.println("sum of integers: " + sum1); // output: sum of integers: 8
+        // Using the add method with integers
+        int sum1 = calculator.add(5, 3); // Calls the first add method (int version)
+        System.out.println("Sum of integers: " + sum1); // Output: Sum of integers: 8
 
-        // using the add method with doubles
-        double sum2 = calculator.add(7.5, 2.5); // calls the second add method (double version)
-        system.out.println("sum of doubles: " + sum2); // output: sum of doubles: 10.0
+        // Using the add method with doubles
+        double sum2 = calculator.add(7.5, 2.5); // Calls the second add method (double version)
+        System.out.println("Sum of doubles: " + sum2); // Output: Sum of doubles: 10.0
     }
 }
