8b86a1ea1c90d0bfd31b7687ec1e7453f974a42a
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-08 10:03:26 +0545

    extra wrapper class

---
+++ packages/Demo1.java
@@ -0,0 +1,12 @@
+public class Demo1 {
+  public static void main(String[] args) {
+    Integer in = 69;
+    System.out.println(in.toString());
+    System.out.println(in.toString().getClass());
+    System.out.println(Integer.toString(69));
+    System.out.println(Integer.toBinaryString(152));
+    System.out.println(Integer.toHexString(152));
+    System.out.println(Integer.toOctalString(152));
+
+  }
+}
