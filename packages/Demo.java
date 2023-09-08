16cba3724328e57006976f2d249cb6591bb66b50
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-08 09:48:30 +0545

    demo of package

---
+++ packages/Demo.java
@@ -0,0 +1,17 @@
+public class Demo {
+  public static void main(String[] args) {
+    double x = 100;
+    double y = 10;
+    System.out.println("maximum number of x and y is: " +Math.max(x, y));
+    System.out.println("square of number of x is: " +Math.sqrt(x));
+    System.out.println("power of number of y with x is: " +Math.pow(y, x));
+    System.out.println("minimium number of x and y is: " +Math.min(y, 9));
+    System.out.println("log number of x is: " +Math.log(x));
+    System.out.println("log number of y is: " +Math.log(y));
+    System.out.println("log number of y is: " +Math.log10(y));
+    System.out.println("log number of x is: " +Math.log10(x));
+    System.out.println("exp number of x is: " +Math.exp(x));
+    System.out.println("exp number of x is: " +Math.expm1(x));
+    
+  }
+}
