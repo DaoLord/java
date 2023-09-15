29e0915d8fff80ddec4a7e3f5fcc4dc36a882cd3
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-15 08:16:56 +0545

    array string container

---
+++ array/One.java
@@ -0,0 +1,8 @@
+public class One {
+  public static void main(String[] args) {
+    String arr[] = {"apple", "mango", "banana"};
+    for(int i = 0; i<3; i++){
+      System.out.println(arr[i]);
+    }
+  }
+}
