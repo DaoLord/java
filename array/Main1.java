4b6e2d581f00a25db10af4e5b6861c6b403c9fa4
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-16 20:44:55 +0545

    dfas

--- array/Main1.java
+++ array/Main1.java
@@ -1,2 +1,8 @@
 import java.util.List;
 import java.util.Iterator;
+public class Main1 {
+  public static void main(String[] args) {
+    List<String> = List.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
+    Iterator
+  }
+}
