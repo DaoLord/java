08b8232e07802d0a45e3534c1d0aa3a167f30c98
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-16 20:45:50 +0545

    dlfaksjdf;lk

--- array/Main1.java
+++ array/Main1.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.Iterator;
 public class Main1 {
   public static void main(String[] args) {
-    List<String> = List.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
-    Iterator
+    List<String> Eing= List.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
+    Iterator<String> obj = Eing.iterator();
   }
 }
