d8fdefe39a08bf33711fa499006fe48f905dfba4
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-16 20:48:36 +0545

    dsafsd

--- array/Main1.java
+++ array/Main1.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.Iterator;
 public class Main1 {
   public static void main(String[] args) {
-    List<String> Eing= List.of(apple, ball, cat, dog);
+    List<String> Eing= List.of("apple", "ball", "cat", "dog");
     Iterator<String> obj = Eing.iterator();
     while(obj.hasNext()){
       String e = obj.next();
