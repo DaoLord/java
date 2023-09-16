a592541de01f22681f2248a60f28cca20acd64f5
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-16 20:46:47 +0545

    dasfdsaf

--- array/Main1.java
+++ array/Main1.java
@@ -4,5 +4,8 @@ public class Main1 {
   public static void main(String[] args) {
     List<String> Eing= List.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
     Iterator<String> obj = Eing.iterator();
+    while(obj.hasNext()){
+      String e = obj.next();
+    }
   }
 }
