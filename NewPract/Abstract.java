ac8c78137dd4fa33dada6184e1c48ac5b72b0504
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-19 18:17:25 +0545

    dafd

---
+++ NewPract/Abstract.java
@@ -0,0 +1,8 @@
+public class Abstract {
+  public static void main(String[] args) {
+    Apple obj = new Apple();
+    obj.setThis("String word"); 
+    String name = obj.getThis();
+    System.out.println(name);
+  }
+}
