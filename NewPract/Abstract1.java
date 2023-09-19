4b5f92ea73930d32508d4dc9af2007cb6dc37eb3
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-19 20:04:31 +0545

    sdfsda

--- NewPract/Abstract1.java
+++ NewPract/Abstract1.java
@@ -1,6 +1,17 @@
 class Dog{
   private String word;
-  public String Rem{
+  public String getWord(){
     return word;
+  }
+  public void setWord(String NewWord){
+    this.word = newWord;
+  }
+}
+public class Abstract1 {
+  public static void main(String[] args) {
+    Dog obj1 = new Dog();
+    obj1.setWord("KOirala Bishal");
+    String word = obj1.getWord();
+    System.out.println(word);
   }
 }
