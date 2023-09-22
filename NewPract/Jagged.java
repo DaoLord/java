c8628458f8a6c6c1490f2b6eff44746738bca1f4
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-22 07:50:01 +0545

    dfasdf

---
+++ NewPract/Jagged.java
@@ -0,0 +1,22 @@
+public class Jagged {
+    public static void main(String[] args) {
+        // Initialize a jagged array of integers
+        int[][] jaggedArray = {
+            {1, 2, 3},
+            {4, 5},
+            {6, 7, 8, 9},
+            {10}
+        };
+
+        // Display the jagged array and calculate the sum of each row
+        System.out.println("Jagged Array:");
+        for (int i = 0; i < jaggedArray.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < jaggedArray[i].length; j++) {
+                System.out.print(jaggedArray[i][j] + " ");
+                rowSum += jaggedArray[i][j];
+            }
+            System.out.println("=> Sum: " + rowSum);
+        }
+    }
+}
