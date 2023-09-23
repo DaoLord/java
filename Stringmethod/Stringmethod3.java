7d5699df43a59ef56efb99d724654c96771ca33a
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-24 04:09:24 +0545

    dfasd

--- Stringmethod/Stringmethod3.java
+++ Stringmethod/Stringmethod3.java
@@ -1,8 +1,8 @@
 public class Stringmethod3
 {
   public static void main(String[]args) {
-   String s1="abc";
-   String s2="dcf";
+   String s1="apple";
+   String s2="ball";
     int out=s1.compareTo(s2);
     System.out.println(out);
     System.out.println(s1.compareToIgnoreCase(s2));//ignorecase in compareTo.
