00443d879eed175f8fa126c8969ec7185bc11cdb
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-24 07:31:38 +0545

    sdafsadf

--- Stringmethod/Stringmethod3.java
+++ Stringmethod/Stringmethod3.java
@@ -1,11 +1,6 @@
 public class Stringmethod3
 {
   public static void main(String[]args) {
-   String s1="apple";
-   String s2="ball";
-    int out=s1.compareTo(s2);
-    System.out.println(out);
-    System.out.println(s1.compareToIgnoreCase(s2));//ignorecase in compareTo.
     String name="bishnu LIMBU";
     System.out.println(name.toLowerCase());
     System.out.println(name.toUpperCase());
