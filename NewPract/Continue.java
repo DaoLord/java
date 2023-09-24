638576f959f6e8592d459a69d74edf26e785483f
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-24 08:08:32 +0545

    dsaafds

--- NewPract/Continue.java
+++ NewPract/Continue.java
@@ -1,7 +1,17 @@
 public class Continue {
   public static void main(String[] args) {
-    for(int i=1;i<11;i++){
-      System.out.print(i );
+  //   for(int i=1;i<11;i++){
+  //     continue;
+  //   }
+  //     System.out.print(i);
+  //   System.out.println("hellow");
+  // }
+for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        // Skip even numbers and continue with the next iteration
+        continue;
     }
+    System.out.println("Odd number: " + i);
+}
   }
 }
