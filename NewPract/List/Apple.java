b176f62827aec2f658cda0994871c686b8fe460e
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-26 08:34:44 +0545

    dfasdf

---
+++ NewPract/List/Apple.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class Apple {
+  public static void main(String[] args) {
+    Queue<Integer> A = new LinkedList<>();
+    A.add(100);
+    A.add(200);
+    A.add(300);
+    A.add(400);
+    System.out.println("Queued Data Output");
+    for(Integer item: queue)
+    System.out.println(num);
+
+  }
+}
