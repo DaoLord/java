88d2fe9c3f8f2a6f9c2fa92c7c65b956cc68344d
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-27 15:22:10 +0545

    final keyword usuage

---
+++ NewPract/Keyword/FinalNum.java
@@ -0,0 +1,12 @@
+class A{
+  void fun(){
+    final int i =10;
+    System.out.println(i);
+  }
+}
+public class FinalNum extends A{
+  public static void main(string[] args) {
+    int i = 100;
+    system.out.println(i);
+  }
+}
