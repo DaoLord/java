e0ba01ec19e9b4bac06dfb54fa5db6f8817cab29
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-07 14:48:03 +0545

    encapsulation

---
+++ javaFloder/Encap.java
@@ -0,0 +1,16 @@
+class Data{
+  String Name;
+  void setName(String s){
+    Name = s;
+  }
+  String getName(){
+    return Name;
+  }
+}
+public class Encap {
+  public static void main(String[] args) {
+    Data d = new Data();
+   d.setName("BishnuLimbu");
+    System.out.println(d.getName());
+  }
+}
