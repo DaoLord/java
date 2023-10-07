9a931953d95fe01c8fe67090a474d57eb8fcd35a
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-07 15:27:17 +0545

    dsafsd

---
+++ javaFloder/StringM.java
@@ -0,0 +1,11 @@
+public class StringM {
+  public static void main(String[] args) {
+    String name = "heroHonda";
+    String secondName = "Motors";
+    System.out.println(name.length());
+    System.out.println(name.charAt(4));
+    System.out.println(name.toUpperCase());
+    System.out.println(name.concat(secondName));
+
+  }
+}
