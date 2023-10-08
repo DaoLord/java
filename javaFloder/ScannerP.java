c74b6f4c71ffb9888eaa93c00db2f0af95c7e524
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-08 07:50:11 +0545

    dfdsf

---
+++ javaFloder/ScannerP.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ScannerP {
+  public static void main(String[] args) {
+    Scanner a = new Scanner(System.in);
+    System.out.println("write");
+    String b = a.nextLine();
+    System.out.println(b);
+    a.close();
+    
+  }
+}
