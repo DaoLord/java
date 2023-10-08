b61b702f60245b952c27e2addc6e9d647775d669
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-08 07:25:11 +0545

    sadfsadf

---
+++ javaFloder/Buffer.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class Buffer {
+  public static void main(String[] args) {
+    StringBuffer st = new StringBuffer("markiV");
+    StringBuffer st1 = st.reverse();
+    System.out.println(st1);
+  }
+}
